49152a5e562d5d111a7131d619b9fa7ea18a175e
Do Le Trong Hieu <hieu.2474802010112@vanlanguni.edu.vn>
2026-06-26 01:13:23 +0700

    feat: support DATABASE_URL in database config

--- felini-backend/src/app.module.ts
+++ felini-backend/src/app.module.ts
@@ -10,16 +10,26 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_NAME || 'felini_db',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(
+      process.env.DATABASE_URL
+        ? {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            autoLoadEntities: true,
+            synchronize: true,
+          }
+        : {
+            type: 'postgres',
+            host: process.env.DB_HOST || 'localhost',
+            port: parseInt(process.env.DB_PORT || '5432'),
+            username: process.env.DB_USERNAME || 'postgres',
+            password: process.env.DB_PASSWORD || 'password',
+            database: process.env.DB_NAME || 'felini_db',
+            autoLoadEntities: true,
+            synchronize: true,
+          }
+    ),
+
     CustomersModule,
     MenuModule,
     ReservationsModule,
